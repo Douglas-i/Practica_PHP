bc56ed6171a946ec088b928a685ad83c72f8b2b1
Douglas-i <hdouglasu@gmail.com>
2023-03-08 14:19:53 -0600

    Hola mundo con PHP

---
+++ 01-hola-mundo.php
@@ -0,0 +1,5 @@
+<?php include 'includes/header.php';
+
+echo "Hola mundo";
+
+include 'includes/footer.php';
--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Introducción a PHP - Código Con Juan</title>
+    <title>Introducción a PHP - Douglas Hernández</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 
