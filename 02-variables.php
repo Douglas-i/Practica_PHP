d03947b3f0ce93689c95adedc3a3f63d6627c6cb
Douglas-i <hdouglasu@gmail.com>
2023-03-27 08:41:14 -0600

    Variables

--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,14 @@
 <?php include 'includes/header.php';
 
+$nombre = "Douglas";
+$_nombre = "Rafael";
 
+echo $nombre;
+
+//Constante
+define('constante', "Este es el valor de la constante");
+
+echo constante;
 
 
 include 'includes/footer.php';
